034ff57f938c19935f32a9e4862b343b43b36b99
renatobeletti <renatobeletti@gmail.com>
2026-02-14 17:32:48 -0300

    Removendo erros do PHP Novo

--- index.php
+++ index.php
@@ -66,8 +66,8 @@
 switch (ENVIRONMENT)
 {
 	case 'development':
-		error_reporting(-1);
-		ini_set('display_errors', 1);
+    	error_reporting(E_ALL & ~E_DEPRECATED & ~E_STRICT);
+    	ini_set('display_errors', 1);
 	break;
 
 	case 'testing':
