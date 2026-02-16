c65f1deaaed2188260a0698f6bd00013fc58a559
Renato Beletti da Silva <renatobeletti@gmail.com>
2026-02-16 10:06:07 -0300

    teste senha pura

--- application/models/Auth_model.php
+++ application/models/Auth_model.php
@@ -2,17 +2,15 @@
 class Auth_model extends CI_Model {
 
     public function login($email, $senha) {
-        $this->db->where('email', $email);
-        $this->db->where('status', 1);
-        $usuario = $this->db->get('usuarios')->row();
+    $this->db->where('email', $email);
+    $usuario = $this->db->get('usuarios')->row();
 
-        if ($usuario) {
-            // Verifica se a senha bate com o hash (BCRYPT)
-            // Se você inseriu a senha como texto puro para testar, use: if($senha == $usuario->senha)
-            if (password_verify($senha, $usuario->senha)) {
-                return $usuario;
-            }
+    if ($usuario) {
+        // TESTE TEMPORÁRIO: Comparação direta (Texto Puro)
+        if ($senha == $usuario->senha) {
+            return $usuario;
         }
-        return FALSE;
     }
+    return FALSE;
+}
 }
